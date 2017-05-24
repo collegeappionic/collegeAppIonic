cd2bd33fbf275af579abb5df0a70dbe2bb1b034d
luhaiyun <orochi430@outlook.com>
2017-05-24 16:56:24 +0800

    22222得到的

--- collegeAppIonic/collegeAppIonic/Global.asax.cs
+++ collegeAppIonic/collegeAppIonic/Global.asax.cs
@@ -10,7 +10,7 @@ namespace collegeAppIonic
     public class WebApiApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
-        {
+        {   
             GlobalConfiguration.Configure(WebApiConfig.Register);
         }
     }
