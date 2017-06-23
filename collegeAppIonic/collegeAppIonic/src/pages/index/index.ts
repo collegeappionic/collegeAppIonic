91c799e0f30fa6ca58c0f481659e487af81ef6b0
zhangwenlang <18305522633@163.com>
2017-06-23 18:06:18 +0800

    123

--- collegeAppIonic/collegeAppIonic/src/pages/index/index.ts
+++ collegeAppIonic/collegeAppIonic/src/pages/index/index.ts
@@ -41,34 +41,55 @@ export class IndexPage {
         this.getMenus().subscribe(
             Data => {
                 this.resData = Data;
-                var datameSource: any[];
+                var datameSource: any[] = [];
                 var pages = (Data.length % 6);
                 //console.log(Data);
                 
                 if ((Data.length % 6) > 0) {
-                    var pages = Math.ceil(Data.length / 6);
-                    console.log(pages);
+                    var pages = Math.floor(Data.length / 6)+1;
+                    //console.log(pages);
                 }else {
-                    var pages = Math.ceil(Data.length / 6)
-                    console.log(pages);
+                    var pages = Math.floor(Data.length / 6)
+                    //console.log(pages);
                 }
-                //for (var i = 0; i < pages; i++) {
-                //    //初始化datapage数据格式
-                //    var dataPage = {
-                //        "index": i,
-                //        "data" : [
-                //            {
-                //                "data": []
-                //            },
-                //            {
-                //                "data": []
-                //            }
-                //        ]
-                //    };
-                //    console.log(dataPage);
-                //    datameSource.push(dataPage);
-                //}
-                console.log(datameSource)
+                for (var i = 0; i < pages; i++) {
+                    //初始化datapage数据格式
+                    var dataPage = {
+                        "index": i,
+                        "menusData" : [
+                            {
+                                "menuData": []
+                            },
+                            {
+                                "menuData": []
+                            }
+                        ]
+                    };
+                    
+                    datameSource.push(dataPage);
+                }
+                var pageIndex: number = 0;
+                var rowIndex: number = 0;
+                var dataRowIndex: number = 0;
+                _.forEach(this.resData, function (m, key) {
+                    
+                    if (pageIndex != Math.floor(key / 6)) {
+                        pageIndex = Math.floor(key / 6);
+                        rowIndex = 0;
+                        dataRowIndex = 0;
+                        console.log(pageIndex);
+                    }
+                    if (rowIndex > 1) {
+                        rowIndex = 0;
+                    }
+                    if (dataRowIndex > 2) {
+                        dataRowIndex = 0;
+                        rowIndex++;
+                    }
+                   
+                });
+                
+                //console.log(datameSource)
             }
        
             //console.log(Data)
